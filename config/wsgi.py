7d16e3e8720a1d01b24036ad480ef97cc6ae8a4f
kcuyh <landwhale@gimhyeogjin-ui-MacBookPro.local>
2019-09-13 15:38:12 +0900

    0.1.1

--- config/wsgi.py
+++ config/wsgi.py
@@ -16,5 +16,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 application = get_wsgi_application()
 
 
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise.storage.CompressedManifestStaticFilesStorage
+
 application = DjangoWhiteNoise(application)
